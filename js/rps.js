c9597b94c1f1e30b99ac8682c16ac1089d204cc1
seokdaseok <64110712+seokdaseok@users.noreply.github.com>
2020-07-18 16:22:12 +0800

    Update rps.js

--- js/rps.js
+++ js/rps.js
@@ -1,28 +1,3 @@
-// var n = Math.random();
-// n = n * 3;
-// var x = Math.random();
-// x = x * 3
-// var playerOne = Math.floor(n + 1);
-// var playerTwo = Math.floor(x + 1);
-//
-// document.querySelector("img").setAttribute("src", "images/rps" + playerOne + ".png")
-// document.querySelectorAll("img")[1].setAttribute("src", "images/rps" + playerTwo + ".png")
-//
-// if (playerOne === playerTwo) {
-//   document.querySelector("h1").innerHTML = "Draw!"
-// } else if (playerOne === 3 && playerTwo === 1) {
-//   document.querySelector("h1").innerHTML = "Player 1 wins!"
-// } else if (playerOne === 1 && playerTwo === 2) {
-//   document.querySelector("h1").innerHTML = "Player 1 wins!"
-// } else if (playerOne === 2 && playerTwo === 3) {
-//   document.querySelector("h1").innerHTML = "Player 1 wins!"
-// } else if (playerTwo === 3 && playerOne === 1) {
-//   document.querySelector("h1").innerHTML = "Player 2 wins!"
-// } else if (playerTwo === 1 && playerOne === 2) {
-//   document.querySelector("h1").innerHTML = "Player 2 wins!"
-// } else if (playerTwo === 2 && playerOne === 3) {
-//   document.querySelector("h1").innerHTML = "Player 2 wins!"
-// }
 
 var playerOne = prompt("Player one, choose. Remember, type all lower case and spell correctly. (rock, paper, scissors)")
 var playerTwo = prompt("Player two, choose. Remember, type all lower case and spell correctly. (rock, paper, scissors)")
